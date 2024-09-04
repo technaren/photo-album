99382d7fa8df5dd61c38a9151a9a90d564b766d2
technaren <narenthiraa@gmail.com>
2024-09-04 20:14:46 +0530

    Add files via upload

---
+++ .next/static/chunks/app_layout_tsx_b9020f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_tsx_b9020f._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_tsx_b9020f._.js",
+  "chunks": [
+    "static/chunks/_01f8d0._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/app_page_tsx_78632b._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_tsx_78632b._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_tsx_78632b._.js",
+  "chunks": [
+    "static/chunks/styles_module_57b763.css",
+    "static/chunks/node_modules_next_dist_a5ccec._.js",
+    "static/chunks/app_page_tsx_9fdd4f._.js"
+  ],
+  "source": "dynamic"
+});
